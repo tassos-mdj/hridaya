e83dc8fbc8a23f62b41fdff9935c5fbcb14d8c17
tassos-mdj <164147506+tassos-mdj@users.noreply.github.com>
2024-04-23 23:12:12 +0300

    feat:Add carousel

--- script.js
+++ script.js
@@ -1,3 +1,46 @@
 function menuTransition(x) {
     x.classList.toggle("change");
   }
+
+  $(document).ready(function(){
+    $('.carousel').slick({
+      
+      variableWidth: true,
+      dots: true,
+      infinite: true,
+      speed: 300,
+      slidesToShow: 4,
+      slidesToScroll: 1,
+      autoplay: true,
+      autoplaySpeed: 3000,
+      responsive: [
+        {
+          breakpoint: 1024,
+          settings: {
+            slidesToShow: 3,
+            slidesToScroll: 1,
+            infinite: true,
+            dots: true
+          }
+        },
+        {
+          breakpoint: 600,
+          settings: {
+            slidesToShow: 2,
+            slidesToScroll: 1
+          }
+        },
+        {
+          breakpoint: 480,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1
+          }
+        }
+        // You can unslick at a given breakpoint now by adding:
+        // settings: "unslick"
+        // instead of a settings object
+      ]
+    });
+  });
+
